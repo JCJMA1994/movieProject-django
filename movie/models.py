d6a99d88e2f500135be1165b53ce93197556ed0a
JCJMA1994 <201214026@uns.edu.pe>
2022-06-18 09:28:51 -0500

    creacion de modelos para las peliculas y directores

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Director(models.Model):
+    name = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=70)
+    date_birth = models.DateField()
+    date_death = models.DateField(null=True, blank=True)
+    biography = models.TextField()
+
+    def __str__(self):
+        return self.name + " " + self.lastname
+
+
+class Movie(models.Model):
+    director = models.ForeignKey('Director', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    synopsis = models.TextField()
+    duration = models.PositiveSmallIntegerField()
+    year = models.PositiveSmallIntegerField()
+
+
+    def __str__(self):
+        return self.name
