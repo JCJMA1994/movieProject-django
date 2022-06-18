10e2078bbe979004642472e2a918edd6d626f5a8
JCJMA1994 <201214026@uns.edu.pe>
2022-06-18 09:34:43 -0500

    creacion de vistas para los modelos Director y Movie

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views import generic
 
+from .models import Director, Movie
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'movie.directors.html'
+    model = Director
+
+
+class DirectorView(generic.DetailView):
+    template_name = 'movie/director.html'
+    model = Director
+
+
+class MovieView(generic.DetailView):
+    template_name = 'movies/movie.html'
+    model = Movie
